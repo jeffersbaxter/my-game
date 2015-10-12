4b0251675c0e000d3dec9f7e83cc0c91f44d173e
jeffersbaxter <jeffersbaxter@comcast.net>
2015-10-12 14:13:49 -0700

    New field with css

--- scripts.js
+++ scripts.js
@@ -1,35 +1,48 @@
 $(document).ready(function(){
 	var start = null;
 	var end = null;
-	var yards = (end - start)+900;
 
 
-	$('body').keydown(function(e){
-		
-		if (e.keyCode==32) {
-			if (!start) {
-				start = new Date();
-				console.log(start);
-			}
-			// alert(start);
-		};
 
-	});
+	var play = function() {
+		$('body').keydown(function(e){
+			
+			if (e.keyCode==32) {
+				if (!start) {
+					start = new Date();
+					console.log(start);
+					moveBall();
+				}
+			};
+		});
+	};
 
-	$('body').keyup(function(e){
-		if (e.keyCode==32) {
-			end = new Date();
-			console.log(end)
-			var yards = (end - start);
-			console.log(yards);
-			start = null;
-			end = null;
-			homePlay();
-		};
-	});
+	var moveBall = function(){
+		$('body').keyup(function(e){
+			if (e.keyCode==32) {
+				end = new Date();
+				console.log(end);
+				var yards = (end - start);
+				console.log(yards);
+				homePlay(yards);
+				start = null;
+				end = null;
+				return yards;
+			};
+		});
+	};
+	var homePlay = function(yds){
+		if (yds > 0 && yds < 75) {
+			$('#ball').css("left", "500px");
+		} else if (yds >= 75 && yds < 150) {
+			$('#ball').css("left", "550px");
+		} else if (yds >= 150 && yds < 350) {
+			$('#ball').css("left", "600px");
+		} else {
+			$('#ball').css("left", "650px");
+		}
 
-	var homePlay = function(){
-		$('.ball').css("left", yards);
 	}
+	play();
 
 });
